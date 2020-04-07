3149e34d82984990913291e4ad320ccbc4118090
xiong <597709174@qq.com>
2020-04-07 11:13:18 +0800

    test

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Link, } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import { connect } from 'react-redux';
 import {getData, addUser} from '@/store/home/action'
 import './index.less';
